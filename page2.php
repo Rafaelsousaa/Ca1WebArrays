ecc771af5118728db3132222345094b8e507786c
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 11:55:37 +0000

    adding a form and  select for get the value inside the array and than a foreach that gives de value

--- page2.php
+++ page2.php
@@ -10,7 +10,21 @@ $country = array(
     "Estonia" => "Tallin", "Hungary" => "Budapest", "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna", "Poland" => "Warsaw"
 
 ); ?>
+<form action="page2.php" method="POST">
+    <select name="x" id="">
+        <option value="" disabled selected> Pick a Country</option>
+        <?php
 
+        foreach ($country as $x => $x_value) {
+            echo "<option value='$x'>$x</option>";
+        }
+        ?>
+
+        <input type="submit" name="submit" value="choose">
+    </select>
+
+
+</form>
 
 
 
