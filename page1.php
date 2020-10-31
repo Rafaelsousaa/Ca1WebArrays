b174c2f58faeede2b92d31fc1953db2f991c3d37
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 11:47:02 +0000

    adding a array country

--- page1.php
+++ page1.php
@@ -6,4 +6,14 @@
             <th scope="col">Capital</th>
         </tr>
 </thead>
+<tbody>
+        <?php
+        $country = array(
+            "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels", "Denmark" => "Copenhagen",
+            "Finland" => "Helsinki", "France" => "Paris", "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
+            "Greece" => "Athens", "Ireland" => "Dublin", "Netherlands" => "Amsterdam", "Portugal" => "Lisbon", "Spain" => "Madrid",
+            "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague",
+            "Estonia" => "Tallin", "Hungary" => "Budapest", "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna", "Poland" => "Warsaw"
+        );
+
 <?php include('Footer.php'); ?>
