6120733acb2f31bc9ada8c9e2ffa0b959fce4519
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 14:26:47 +0000

    explanation about page1

--- page1.php
+++ page1.php
@@ -1,4 +1,6 @@
+<!-- in this parte I include my header by using php include -->
 <?php include('Header.php'); ?>
+<!-- I created a table a give some style in bootstrap -->
 <table class="table table-striped table-dark">
     <thead>
         <tr>
@@ -8,6 +10,7 @@
     </thead>
     <tbody>
         <?php
+        // for this parte i used a array with the countries that i need to use it insede the table
         $country = array(
             "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels", "Denmark" => "Copenhagen",
             "Finland" => "Helsinki", "France" => "Paris", "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
@@ -15,13 +18,15 @@
             "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague",
             "Estonia" => "Tallin", "Hungary" => "Budapest", "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna", "Poland" => "Warsaw"
         );
+        // in here i used a for lopp to get the value $x as a country and $x_value to get the key that is the capital 
         foreach ($country as $x => $x_value) {
-
+// in here i printed the country as $x amd the capital as $x-value
             echo "<tr><td>$x</td><td>$x_value</td></tr>";
         }
 
         ?>
         </tbody>   
     </table>
+    <!-- this parte I include the footer -->
 
         <?php include('Footer.php'); ?>
