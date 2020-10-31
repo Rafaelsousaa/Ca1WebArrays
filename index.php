e8eca7bf9b27048830ab7de7bde7d56a306f4b51
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 14:15:53 +0000

    explanation about my clas text

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <!-- including the header by using php include -->
 <?php include('Header.php'); ?>
-
+<!-- in here i put my pensonal information wih class text, I edited the size and the positon -->
 <div class="text">
     <h1><strong>Rafael Alcantara de sousa</strong></h1>
     <h2>Student Number - 2019335</h2>
